43340add3bbbbaec4af56fe58e1ff6f06f6cef96
Daniel Gacitúa <daniel.gacitua@usach.cl>
2016-09-30 11:27:31 -0300

    Fixing presentation bugs

--- client/main.js
+++ client/main.js
@@ -1,8 +1,6 @@
 import angular from 'angular';
 import angularMeteor from 'angular-meteor';
 
-import Locale from './lib/locale'
-
 import { name as App } from '../imports/components/app/app';
 
 angular.module('prototype2', [
