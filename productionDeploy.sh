57360adc8fe597a9ffb4314a468feac9f73a969c
Daniel Gacitua <daniel.gacitua@usach.cl>
2016-11-12 17:20:06 -0300

    Fixes to Travis CI deploy

--- productionDeploy.sh
+++ productionDeploy.sh
@@ -22,18 +22,18 @@ echo ">> Creating offline image..."
 docker save dgacitua/neurone > neuroneimage.tar
 
 echo ">> Copying files to remote host..."
-scp dockerDeploy.sh $USER@$HOST:$WORKDIR
-scp neuroneimage.tar $USER@$HOST:$WORKDIR
+sshpass -e scp dockerDeploy.sh $USER@$HOST:$WORKDIR
+sshpass -e scp neuroneimage.tar $USER@$HOST:$WORKDIR
 
 echo ">> Removing local offline image..."
 rm neuroneimage.tar
 
 echo ">> Loading offline image in remote host..."
-ssh $USER@$HOST docker load < neurone-app.tar
-ssh $USER@$HOST rm $WORKDIR/neuroneimage.tar
+sshpass -e ssh $USER@$HOST docker load < neurone-app.tar
+sshpass -e ssh $USER@$HOST rm $WORKDIR/neuroneimage.tar
 
 echo ">> Deploy MongoDB database in remote host..."
-ssh $USER@$HOST docker run -d \
+sshpass -e ssh $USER@$HOST docker run -d \
                 -p $NEURONE_DB_PORT:27017 \
                 -v $NEURONE_DB_PATH:/data/db \
                 --name $NEURONE_DB_NAME  \
@@ -41,12 +41,12 @@ ssh $USER@$HOST docker run -d \
                 mongo --auth
 
 echo ">> Setting authentication to database in remote host..."
-ssh $USER@$HOST "docker exec -t $NEURONE_DB_NAME mongo admin --eval \\
+sshpass -e ssh $USER@$HOST "docker exec -t $NEURONE_DB_NAME mongo admin --eval \\
                 \"db.createUser({ user: \\\"$NEURONE_DB_USER\\\", pwd: \\\"$NEURONE_DB_PASS\\\", \\
                 roles: [{ role: \\\"userAdminAnyDatabase\\\", db: \\\"admin\\\" }]})\""
 
 echo ">> Deploy offline NEURONE image in remote host..."
-ssh $USER@$HOST docker run -d \
+sshpass -e ssh $USER@$HOST docker run -d \
                 -e ROOT_URL="http://$HOST" \
                 -e MONGO_URL="mongodb://$NEURONE_DB_USER:$NEURONE_DB_PASS@$NEURONE_DB_NAME:$NEURONE_DB_PORT/$NEURONE_MONGO_DATABASE" \
                 -p $NEURONE_APP_PORT:80 \
