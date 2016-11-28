c598673e32bbdbedb1978b7425d2389157dbf376
Daniel Gacitua <daniel.gacitua@usach.cl>
2016-11-28 13:16:42 -0300

    Add online/away/offline event log tracking

--- server/connectionTracker.js
+++ server/connectionTracker.js
@@ -2,23 +2,49 @@ import Utils from './lib/utils';
 
 import { FlowLogs } from '../imports/api/flowLogs/index';
 import { FlowSessions } from '../imports/api/flowSessions/index';
+import { EventLogs } from '../imports/api/eventLogs/index';
 
 UserPresenceMonitor.onSetUserStatus((user, status, statusConnection) => {
   var userId = user._id,
-    username = user.emails[0].address,
+    username = user.username || user.emails[0].address,
    timestamp = Utils.getTimestamp(),
-        time = new Date(timestamp);
+        date = Utils.timestamp2date(timestamp),
+        time = Utils.timestamp2time(timestamp),
+      action = '';
 
-  if (status === 'online') {
-    //console.log('User Online!', userId, username, status, time);
+  if (status === 'away') {
+    action = 'StatusAway';
   }
   else if (status === 'offline') {
-    //console.log('User Offline!', userId, username, status, time);
+    action = 'StatusOffline';
   }
-  else if (status === 'away') {
-    //console.log('User Away!', userId, username, status, time);
+  else if (status === 'online') {
+    action = 'StatusOnline';
   }
   else {
-    //console.log('User status change!', userId, username, status, time);
+    action = 'StatusChange';
+  }
+
+  var action = {
+    userId: userId,
+    username: username,
+    action: action,
+    actionId: '',
+    clientDate: date,
+    clientTime: time,
+    clientTimestamp: timestamp,
+    serverDate: date,
+    serverTime: time,
+    serverTimestamp: timestamp,
+    ipAddr: '',
+    userAgent: '',
+    extras: ''
+  };
+
+  try {
+    EventLogs.insert(action);
+  }
+  catch (err) {
+    throw new Meteor.error('DatabaseError', 'Could not save in Database!');
   }
 });
