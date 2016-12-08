abe00bcc4286f5db067c97abed5beea46edf826f
Daniel Gacitua <daniel.gacitua@usach.cl>
2016-12-08 18:04:17 -0300

    Add nested views for Stage 2

--- imports/components/session/stages/stage2.js
+++ imports/components/session/stages/stage2.js
@@ -17,7 +17,7 @@ class Stage2 {
   }
 
   deleteSnippet(index) {
-    
+    console.log('Delete Snippet', index);
   }
 }
 
@@ -31,24 +31,58 @@ export default angular.module(name, [
   controllerAs: name,
   controller: Stage2
 })
+.component('pageview', {
+  templateUrl: 'stage2/pageview.html',
+  controllerAs: name,
+  controller: Stage2
+})
+.component('snippetbar', {
+  templateUrl: 'stage2/snippetbar.html',
+  controllerAs: name,
+  controller: Stage2
+})
 .config(config);
 
 function config($stateProvider) {
   'ngInject';
 
+  // dgacitua: http://stackoverflow.com/a/37964199
   $stateProvider
     .state('stage2', {
-      url: '/stage2?page',
       template: '<stage2></stage2>',
       resolve: {
-      currentUser($q) {
-        if (Meteor.userId() === null) {
-          return $q.reject('AUTH_REQUIRED');
-        }
-        else {
-          return $q.resolve();
+        currentUser($q) {
+          if (Meteor.userId() === null) {
+            return $q.reject('AUTH_REQUIRED');
+          }
+          else {
+            return $q.resolve();
+          }
         }
       }
-    }
-  });
+    })
+    .state('stage2.parts', {
+      url: '/stage2',
+      views: {
+        'pageview@stage2': {
+          template: '<pageview></pageview>'
+        },
+        'snippetbar@stage2': {
+          template: '<snippetbar></snippetbar>'
+        }
+      }
+    })
+    /*
+    .state('stage2.pageview', {
+      url: '/pageview',
+      templateUrl: 'stage2/pageview.html',
+      controller: Stage2
+    })
+    .state('stage2.snippetbar', {
+      url: '/snippetbar',
+      templateUrl: 'stage2/snippetbar.html',
+      controller: Stage2
+    })
+    */
+    ;
 };
