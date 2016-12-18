6dfeefb2e48af03804d89a483e5d9541e5dfabdd
Daniel Gacitua <daniel.gacitua@usach.cl>
2016-12-17 21:50:42 -0300

    Update SharedSettings with placeholder values

--- imports/sharedSettings.js
+++ imports/sharedSettings.js
@@ -1,7 +1,34 @@
 export default SharedSettings = {
   locale: 'fi',
+  queryIdeas: 2,
+  minBookmarks: 3,
   maxBookmarks: 3,
+  minSnippetsPerPage: 1,
+  maxSnippetsPerPage: 3,
+  minSnippetWordLength: 5,
+  maxSnippetWordLength: 50,
+  snippetTruncateThreshold: 25,
+  minSynthesisWordLength: 50,
+  syhtesisAutosaveInterval: 30,
+  stageLength: [5, 10, 10, 10],
+  // Legacy configs
   snippetsPerPage: 3,
-  snippetLength: 15,
-  stageLength: [10, 10]
+  snippetLength: 15
 }
+
+/*
+  Query Formulation Ideas [2]
+  Min Bookmarks [3]
+  Max Bookmarks [3]
+  Min Snippet Count (per page) [1]
+  Max Snippet Count (per page) [3]
+  Min Snippet Word Lenght [5]
+  Max Snippet Word Lenght [50]
+  Snippet Word Truncate Threshold [25]
+  Min Synthesis Report Word Lenght [50]
+  Synthesis Autosave Interval (seconds) [30]
+  Stage0 Time Length (minutes) [?]
+  Stage1 Time Length (minutes) [?]
+  Stage2 Time Length (minutes) [?]
+  Stage3 Time Length (minutes) [?]
+*/
