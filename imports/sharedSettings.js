bec0c0a76dc2edd333c4fd843a0819800a3ede52
Daniel Gacitua <daniel.gacitua@usach.cl>
2016-12-17 21:55:28 -0300

    More SharedSettings Updates

--- imports/sharedSettings.js
+++ imports/sharedSettings.js
@@ -1,5 +1,7 @@
 export default SharedSettings = {
   locale: 'fi',
+  task: availableTasks[0],
+  topics: availableTopics[0],
   queryIdeas: 2,
   minBookmarks: 3,
   maxBookmarks: 3,
@@ -7,7 +9,7 @@ export default SharedSettings = {
   maxSnippetsPerPage: 3,
   minSnippetWordLength: 5,
   maxSnippetWordLength: 50,
-  snippetTruncateThreshold: 25,
+  snippetWordTruncateThreshold: 25,
   minSynthesisWordLength: 50,
   syhtesisAutosaveInterval: 30,
   stageLength: [5, 10, 10, 10],
@@ -16,19 +18,13 @@ export default SharedSettings = {
   snippetLength: 15
 }
 
-/*
-  Query Formulation Ideas [2]
-  Min Bookmarks [3]
-  Max Bookmarks [3]
-  Min Snippet Count (per page) [1]
-  Max Snippet Count (per page) [3]
-  Min Snippet Word Lenght [5]
-  Max Snippet Word Lenght [50]
-  Snippet Word Truncate Threshold [25]
-  Min Synthesis Report Word Lenght [50]
-  Synthesis Autosave Interval (seconds) [30]
-  Stage0 Time Length (minutes) [?]
-  Stage1 Time Length (minutes) [?]
-  Stage2 Time Length (minutes) [?]
-  Stage3 Time Length (minutes) [?]
-*/
+const availableTasks = [
+  'pre',
+  'post'
+];
+
+const availableTopics = [
+  'pilot',
+  'science',
+  'social'
+];
