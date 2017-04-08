bf35f502d1560460c5d4bc4f1c8be7639d950226
Daniel Gacitua <daniel.gacitua@usach.cl>
2017-04-08 19:53:01 -0300

    Fix development deploy params

--- developmentDeploy.sh
+++ developmentDeploy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-export LC_ALL=en_US.utf8
+export LC_ALL=C.UTF-8
 
-export NEURONE_ASSET_PATH="/home/dgacitua/ProyectoTitulo/neuroneDocs"
+export NEURONE_ASSET_PATH=$(cd ../assets; pwd)
 export NEURONE_SOLR_HOST="127.0.0.1"
 export NEURONE_SOLR_PORT="8983"
 export NEURONE_SOLR_CORE="neurone"
