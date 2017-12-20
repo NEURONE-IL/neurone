c6b615f99589d89a115d8dbca27ee9ce244036ec
Daniel Gacitua <dgacitua@dgacitua-LAPTOP.lan>
2017-12-20 16:11:37 -0300

    Fixing createUser and entrypoint scripts for Docker deploy

--- .deploy/docker/createUser.sh
+++ .deploy/docker/createUser.sh
@@ -3,6 +3,6 @@
 USER_NAME=${LOCAL_USER_NAME:-user}
 USER_ID=${LOCAL_USER_ID:-9001}
 
-printf "%b\n" "\e[1;31m>> Starting application with USER: $USER_NAME (UID: $USER_ID)\e[0m"
+printf "%b\n" "\e[1;31m>> Creating user for NEURONE with name: $USER_NAME (UID: $USER_ID)\e[0m"
 useradd --shell /bin/bash -u $USER_ID -o -c "" -m $USER_NAME
 export HOME="/home/$USER_NAME"
--- .deploy/docker/entrypoint.sh
+++ .deploy/docker/entrypoint.sh
@@ -7,8 +7,4 @@
 USER_NAME=${LOCAL_USER_NAME:-user}
 USER_ID=${LOCAL_USER_ID:-9001}
 
-printf "%b\n" "\e[1;31m>> Starting application with USER: $USER_NAME (UID: $USER_ID)\e[0m"
-useradd --shell /bin/bash -u $USER_ID -o -c "" -m $USER_NAME
-export HOME="/home/$USER_NAME"
-
 exec /usr/local/bin/gosu $USER_NAME "$@"
