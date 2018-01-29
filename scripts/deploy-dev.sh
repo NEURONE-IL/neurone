2cab8ecd459e09aa319c88cefe3ef4ab5deb8947
Daniel Gacitua <dgacitua@dgacitua-DESKTOP.lan>
2018-01-29 17:59:49 -0300

    Add Dev Asset directory as subfolder of project

--- scripts/deploy-dev.sh
+++ scripts/deploy-dev.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+ASSET_PATH="./assets"
+mkdir -p $ASSET_PATH
+
 printf "\e[1;32m>>\e[31m Exporting Locales for MongoDB... \e[1;32m<<\e[0m\n"
 export LC_ALL=C.UTF-8
 
 printf "\e[1;32m>>\e[31m Exporting Asset Path for NEURONE... \e[1;32m<<\e[0m\n"
-export NEURONE_ASSET_PATH=$(cd ../assets; pwd)
+export NEURONE_ASSET_PATH=$(cd $ASSET_PATH; pwd)
 
 printf "\e[1;32m>>\e[31m Exporting local Solr Enviroment Variables... \e[1;32m<<\e[0m\n"
 export NEURONE_SOLR_HOST="127.0.0.1"
