a71148c785d360a07cb0935470396d07d3c023a1
Jose Mellado <jose.mellado@usach.cl>
2020-11-24 00:36:40 -0300

    ADD: create domains with API

--- server/externalServices/api.js
+++ server/externalServices/api.js
@@ -2,6 +2,7 @@
 
 import DocumentRetrieval from '../documentIndexer/documentRetrieval';
 import DocumentDownloader from '../documentIndexer/documentDownloader';
+import { FlowComponents } from '../../imports/database/flowComponents/index';
 
 // jmellado: https://stackoverflow.com/questions/36002493/no-access-control-allow-origin-header-in-angular-2-app
 WebApp.rawConnectHandlers.use(function(req, res, next) {
@@ -50,6 +51,14 @@ WebApp.connectHandlers.use('/v1/ping', (req, res, next) => {
   res.end(parseResponse(response)); 
 });
 
+WebApp.connectHandlers.use('/v1/xd', (req, res, next) => {
+  let flow = FlowComponents.findOne({name: "domain5"});
+  console.log(flow == null)
+
+  res.writeHead(200, { 'Content-Type': 'application/json' });
+  res.end(parseResponse(flow)); 
+});
+
 WebApp.connectHandlers.use('/v1/document/search', async (req, res, next) => {
   try {
     let queryObj = await parseBody(req);
@@ -66,8 +75,15 @@ WebApp.connectHandlers.use('/v1/document/search', async (req, res, next) => {
 });
 
 WebApp.connectHandlers.use('/v1/document/load', async (req, res, next) => {
+  let docObj = await parseBody(req);
+  let domain = docObj.domain[0];
+  let flow = await FlowComponents.findOne({name: domain, type: "domain"});
+  if(flow == null){
+    await FlowComponents.insert({name: domain, type: "domain"});
+    await FlowComponents.insert({name: domain, type: "task"});
+    await FlowComponents.insert({name: domain, type: "locale"});
+  }
   try {
-    let docObj = await parseBody(req);
     DocumentDownloader.fetch(docObj, (err, response) => {
       if(!err){
         res.writeHead(200, { 'Content-Type': 'application/json' });
