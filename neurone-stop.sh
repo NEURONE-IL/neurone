fd0252b9960d1cba35094cf6a6abbf2413e1ed8e
Cristobal <cristobal.tbecerraf@gmail.com>
2021-04-05 22:18:40 -0400

    fixed scripts

--- neurone-stop.sh
+++ neurone-stop.sh
@@ -5,7 +5,7 @@ set -e
 
 # Load environment variables from file
 set -o allexport
-source dotenv
+source ./dotenv
 set +o allexport
 
 # Stop with Docker Compose after all environment variables are declared
